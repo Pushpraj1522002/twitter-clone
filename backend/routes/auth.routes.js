5bf217ae06958e0f448a0eb509b3d23436130d35
Pushpraj1522002 <pushpraj.desai.1763@gmail.com>
2025-04-16 23:17:20 +0530

    1-auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import {signup, login, logout} from '../controllers/auth.controller.js'
+import {signup, login, logout, getMe} from '../controllers/auth.controller.js'
+import { protectRoute } from '../middleware/protecteRoute.js';
 
 const router = express.Router();
 
@@ -22,7 +23,8 @@ router.post('/logout', async (req, res)=>{
 }); */
 
 
-router.get('/signup', signup);
+router.get('/me', protectRoute, getMe);
+router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from 'dotenv';
-import authRouter from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +14,12 @@ dotenv.config();
 
 // console.log(process.env.MONGO_URI)
 
-app.use('/api/auth', authRouter)
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({extended:true})); //to parse url data (urlencoded)
+
+app.use(cookieParser()); // in middleware protectRoute to parse the cookie for authentication of other function rather than logout/login/signin
+
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () =>{
     console.log(`Server is runing on port ${PORT}`)
